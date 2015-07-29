edcf7e1535b061569d363dd063e8ebf686b28864
于亚杰 <281806868@qq.com>
2015-07-29 19:40:38 +0800

    ll1

--- initial/src/main/java/ddhy/AppRun.java
+++ initial/src/main/java/ddhy/AppRun.java
@@ -2,9 +2,15 @@ package ddhy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @SpringBootApplication
-public class AppRun {
+public class AppRun extends SpringBootServletInitializer{
+	 @Override
+	 protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	        return application.sources(AppRun.class);
+	    }
 	public static void main(String[] args) {
         SpringApplication.run(AppRun.class, args);
     }
